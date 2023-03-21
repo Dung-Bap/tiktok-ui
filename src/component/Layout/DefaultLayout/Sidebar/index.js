276a8af1e584301616d26106f2398a234cf41320
Dung_Bap <ngovandung397@gmail.com>
2023-03-21 17:56:13 +0700

    Dung khung layout mac dinh

--- src/component/Layout/DefaultLayout/Sidebar/index.js
+++ src/component/Layout/DefaultLayout/Sidebar/index.js
@@ -1,7 +1,10 @@
+import styles from './Sidebar.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
 function Sidebar() {
-    return ( 
-        <h2> Sidebar </h2>
-     );
+    return <h2 className={cx('wrapper')}>Sidebar</h2>;
 }
 
 export default Sidebar;
