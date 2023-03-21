4fd486cc64f1e2b8804224ded91843425fdb6c26
Dung_Bap <ngovandung397@gmail.com>
2023-03-21 14:19:27 +0700

    Change git 1

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+// Pages
+import HeaderOnLy from '~/component/Layout/HeaderOnly';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+
+// Routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnLy },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
