b053000fbcd445eb9d06b3370b801c580f4a2b81
Dung_Bap <ngovandung397@gmail.com>
2023-03-22 14:49:59 +0700

    change css header

--- src/component/Layout/components/Header/index.js
+++ src/component/Layout/components/Header/index.js
@@ -1,13 +1,32 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle, faSpinner, faSearch } from '@fortawesome/free-solid-svg-icons';
+
+import images from '~/assets/images';
 import styles from './Header.module.scss';
 
 const cx = classNames.bind(styles);
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            
-        </div>
-    </header>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo.default} alt="TikTok"></img>
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accout and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faTimesCircle} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faSearch} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
